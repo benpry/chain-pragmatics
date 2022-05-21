b50f749452d0e2c708b1a1dbf23338876ca8e994
Ben Prystawski <11021815+benpry@users.noreply.github.com>
2022-05-21 14:18:12 -0700

    added cache dir for tokenizer too

--- code/test_gptj.py
+++ code/test_gptj.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
         "EleutherAI/gpt-j-6B", revision="float16", torch_dtype=torch.float16, low_cpu_mem_usage=True,
         cache_dir="/data3/benpry/transformers-cache"
     )
-    tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
+    tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B", cache_dir="/data3/benpry/transformers-cache")
 
     gen = pipeline("text-generation", model=model, tokenizer=tokenizer)
     print(gen("Hi my name is Ben and"))
