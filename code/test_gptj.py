bbbb38fc1a9e89653ef36087fa1a8a5cb53d3289
Ben Prystawski <11021815+benpry@users.noreply.github.com>
2022-05-21 13:42:43 -0700

    set transformer cache to not be in my home directory

--- code/test_gptj.py
+++ code/test_gptj.py
@@ -4,6 +4,10 @@ This code tests using GPT-J locally
 import torch
 from transformers import AutoTokenizer, GPTJForCausalLM, pipeline
 
+# don't save the transformers in my home directory
+import os
+os.environ["TRANSFORMERS_CACHE"] = "/data3/benpry/transformers-cache"
+
 if __name__ == "__main__":
     model = GPTJForCausalLM.from_pretrained(
         "EleutherAI/gpt-j-6B", revision="float16", torch_dtype=torch.float16, low_cpu_mem_usage=True
